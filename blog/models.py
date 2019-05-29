c1eae94a101e5ba0f235d51a773fae955bc8461a
Shane Curtis <scurtis9@gmail.com>
2019-05-29 01:30:49 -0600

    changed static location

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils import timezone
 # from django.contrib.auth.models import User
-from jbosite import settings
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
@@ -16,7 +19,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     categories = models.ManyToManyField("Category", related_name="posts")
 
     def __str__(self):
